50967ba1abc2f20410ecae1c2ff1688692b3bfe1
Luis Londono <lulondono08@gmail.com>
2025-09-20 14:07:15 -0500

    test route

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import "./App.css";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 import Home from "./components/Home";
+import Test from "./components/Test";
 
 const App = () => {
   return (
     <Router>
       <Routes>
         <Route index element={<Home />} />
+        <Route path="/test" element={<Test />} />
       </Routes>
     </Router>
   );
---
+++ src/components/Test.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Test = () => {
+  return (
+    <div>
+      <h1>Welcome to the Test Page</h1>
+      <p>This is a test page for the application.</p>
+      <p>Feel free to explore the different sections.</p>
+      <p>Check back often for updates!</p>
+      <p>Enjoy your stay!</p>
+    </div>
+  );
+};
+
+export default Test;
