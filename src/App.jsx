366316f3b0394be265f11b4fec88e4491b13e7d3
Luis Londono <lulondono08@gmail.com>
2025-09-20 14:01:09 -0500

    correct path

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import "./App.css";
 
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
-import Home from "./pages/Home";
+import Home from "./components/Home";
 
 const App = () => {
   return (
