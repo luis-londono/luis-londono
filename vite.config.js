bad6dadfadb853b8832c041986c001171bda0e00
Luis Londono <lulondono08@gmail.com>
2025-09-20 14:14:55 -0500

    Configure Vite base and add GH Pages deploy

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  // Base public path when served in production (use your repo name for project pages)
+  base: '/luis-londono/',
   plugins: [react()],
 })
